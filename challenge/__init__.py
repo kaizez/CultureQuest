873bf2ddc15575a6f4881f55517607d863bcc78c
Ayden000 <yhlee.ayden@gmail.com>
2025-08-04 20:57:31 +0800

    edited challenge route

--- challenge/__init__.py
+++ challenge/__init__.py
@@ -1,6 +1,2 @@
 # Challenge module
-from flask import Blueprint
-
-challenge_bp = Blueprint('challenge', __name__)
-
-# Define routes here or import them from other files if needed.
+from .challenge import challenge_bp
